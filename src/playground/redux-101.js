23c5c92f84cdb3d4d77c2bddb5d7623afaeee9fd
Kristenn <kquemener@yahoo.com>
2019-02-27 19:51:20 -0600

    destructuring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,13 +1,14 @@
 import { createStore } from 'redux'
 
 const store = createStore((state = { count: 0 }, action) => {
+  const { incrementBy = 1, decrementBy = 1, count = state.count } = action
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return { count: state.count + incrementBy }
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy ==='number' ? action.decrementBy : 1
       return { count: state.count - decrementBy }
+    case 'SET':
+      return { count }
     case 'RESET':
       return { count: 0 }
     default:
@@ -25,7 +26,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'INCREMENT',
+  type: 'INCREMENT'
 })
 
 store.dispatch({
@@ -39,6 +40,11 @@ store.dispatch({
 
 store.dispatch({
   type: 'DECREMENT'
+})
+
+store.dispatch({
+  type: 'SET',
+  count: 101
 })
 
 unsubscribe()
